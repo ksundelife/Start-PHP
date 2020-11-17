d9489d810f2773aba7663681a25cc4288f885b37
Ksusha <67238837+ksundelife@users.noreply.github.com>
2020-11-17 23:36:49 +0300

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,25 @@
+<?php 
+
+require DOCROOT . '/engine/db.php'; 
+
+if(isset($_GET['id']) && is_numeric($_GET['id'])){
+
+	$item = dbGetRow('select * from imges where id = ' . (int)$_GET['id'] . ';');
+   // $click = dbGetRow('UPDATE imges SET click = click + 1 where id = '. (int)$_GET['id']  .';');
+	if(!$item){
+		abort(404);
+	}
+    $title = 'Креслo-качалкa МИ';
+    $content = view('pages/gallery_item', $item);
+    var_dump($item);
+	return require TPL_PATH . 'layout.php';
+
+}
+
+$title = 'Кресла-качалки МИ';
+
+$data = dbGetAll('select * from imges;');
+
+$content = view('pages/gallery', ['imges' => $data]);
+
+require TPL_PATH . 'layout.php';
