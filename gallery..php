1eabbbd4d91dad36a2d1585dad7cce9c7f629e83
Ksusha <67238837+ksundelife@users.noreply.github.com>
2020-11-28 00:58:47 +0300

    Delete gallery..php

--- gallery..php
+++
@@ -1,5 +0,0 @@
-<div class="gallery">
-    <?php foreach ($imges as $item): ?>
-        <a href="/gallery/?id=<?= $item['id'] ?>" target = "_blank" ><img src="<?= $item['url'] ?>" style="width:200px"></a>
-    <?php endforeach; ?>
-</div>
